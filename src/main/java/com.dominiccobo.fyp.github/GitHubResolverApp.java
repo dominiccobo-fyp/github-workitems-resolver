1cf3ec68384e526f595fbc23cf76232ff113fc82
Dominic Cobo <dominiccobo@gmail.com>
2019-10-11 10:38:16 +0100

    Add GitHub URL resolution logic

--- src/main/java/com.dominiccobo.fyp.github/GitHubResolverApp.java
+++ src/main/java/com.dominiccobo.fyp.github/GitHubResolverApp.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ApplicationEntryPoint {
+public class GitHubResolverApp {
 
     public static void main(String[] args) {
-        SpringApplication.run(ApplicationEntryPoint.class, args);
+        SpringApplication.run(GitHubResolverApp.class, args);
     }
 }
--- src/main/java/com.dominiccobo.fyp.github/SampleResolver.java
+++ src/main/java/com.dominiccobo.fyp.github/SampleResolver.java
@@ -1,7 +1,7 @@
 package com.dominiccobo.fyp.github;
 
-import com.dominiccobo.fyp.api.events.GitHubUpstreamResolved;
-import org.axonframework.eventhandling.EventHandler;
+import com.dominiccobo.fyp.api.queries.GetWebAppForUpstreamUrl;
+import org.axonframework.queryhandling.QueryHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
@@ -9,10 +9,25 @@ import org.springframework.stereotype.Component;
 @Component
 public class SampleResolver {
 
-    public static final Logger LOG = LoggerFactory.getLogger(SampleResolver.class);
+    private static final Logger LOG = LoggerFactory.getLogger(SampleResolver.class);
 
-    @EventHandler
-    public void onEvent(GitHubUpstreamResolved resolved) {
-        LOG.info("Received event {}", resolved.url);
+    @QueryHandler()
+    public GetWebAppForUpstreamUrl.Result on(GetWebAppForUpstreamUrl query) {
+        LOG.info("Received query for {}", query.upstreamUrl);
+
+        if(isGitHub(query.upstreamUrl)) {
+            return new GetWebAppForUpstreamUrl.Result(resolveSSHUrlToWebView(query.upstreamUrl));
+        }
+        else return null;
+    }
+
+    static boolean isGitHub(String testString) {
+        return testString.contains("git@github.com");
+    }
+
+    static String resolveSSHUrlToWebView(String url) {
+        String pathDir = url.split("git@github.com:")[1];
+        pathDir = pathDir.replaceAll(".git", "");
+        return "https://github.com/" + pathDir;
     }
 }
